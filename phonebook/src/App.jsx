9ef3f213fa42d74d9a243ee6a07c8bdfb7ad10c4
Arif Setiawan <arifsetwn@gmail.com>
2025-04-15 11:26:07 +0700

    phonebook
    phonebook

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons([...persons, newPerson])
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          number: <input 
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+          <div key={index}>
+            {person.name} {person.number}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
