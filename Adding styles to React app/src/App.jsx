8cee8f75b563707ee07389b75aae67ea83ba1954
Arif Setiawan <arifsetwn@gmail.com>
2025-05-25 22:08:12 +0700

    update

--- Adding styles to React app/src/App.jsx
+++ Adding styles to React app/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
 
   useEffect(() => {
@@ -41,6 +42,18 @@ const App = () => {
 
   }
 
+  const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
@@ -54,9 +67,13 @@ const App = () => {
       setNotes(notes.map(note => note.id === id ? returnedNote : note))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
-      )
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+
       setNotes(notes.filter(n => n.id !== id))
     })
 }
@@ -64,6 +81,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
